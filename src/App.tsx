c93d88ecdd29e8f11a203c8298caf66d57be7406
thorntonwill <thorntonwilliam17@gmail.com>
2025-05-13 12:04:50 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-// @ts-ignore - Ignore TypeScript errors for the MovieMatch import
-import MovieMatch from "./MovieMatch";
+import { useState } from 'react';
+// Import your components
+import MovieMatch from './MovieMatch';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 py-8">
+    <div className="App">
       <MovieMatch />
     </div>
   );
